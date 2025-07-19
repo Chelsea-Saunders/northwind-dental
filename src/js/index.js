e17f764ddf292fff6f71805db9e3b137360d7371
Chelsea-Saunders <u605021716@byui.edu>
2025-07-18 20:46:05 -0600

    edited hero video for larger screen, working on parallax effect and .js on it.

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,16 @@
 
+{/* <script>
+    document.addEventListener("DOMContentLoaded", function () {
+        const toggle = document.querySelector(".read-more");
+        const moreText = document.querySelector(".more-text");
+
+        toggle.addEventListener("click", function () {
+            moreText.classList.toggle("hidden");
+            toggle.style.display = "none";
+        });
+    });
+</script> */}
+
 document.addEventListener("DOMContentLoaded", () => {
     const speaker = document.querySelector(".speaker-icon");
 
