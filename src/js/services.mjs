55d28bb10594048390c46e15604aef84285a0c5d
Chelsea-Saunders <168315598+Chelsea-Saunders@users.noreply.github.com>
2025-08-09 12:20:59 -0600

    buttons darker

--- src/js/services.mjs
+++ src/js/services.mjs
@@ -7,31 +7,31 @@ const servicesOfferedModal = `
             <div class="circle-img-wrapper open-modal">
                 <img src="src/images/img-links/smile.png" alt="Image of a bright smile" class="preventative-smile">
             </div>
-            <h3>Preventative Care</h3>
+            <h3 class="serv-h3">Preventative Care</h3>
         </li>
         <li>
             <div class="circle-img-wrapper open-modal">
                 <img src="src/images/img-links/implants.png" alt="Image of a dental implant bridge" class="restorative">
             </div>
-            <h3>Restorative Care</h3>
+            <h3 class="serv-h3">Restorative Care</h3>
         </li>
         <li>
             <div class="circle-img-wrapper open-modal">
                 <img src="src/images/img-links/emergency.png" alt="Image of a person with a toothache" class="emergency">
             </div>
-            <h3>Emergency Care</h3>
+            <h3 class="serv-h3">Emergency Care</h3>
         </li>
         <li>
             <div class="circle-img-wrapper open-modal">
                 <img src="src/images/img-links/whitening.png" alt="Image of digitally whitened teeth" class="whitening">
             </div>
-            <h3>Cosmetic Dentistry</h3>
+            <h3 class="serv-h3">Cosmetic Dentistry</h3>
         </li>
         <li>
             <div class="circle-img-wrapper open-modal">
                 <img src="src/images/img-links/dentures.png" alt="Image of dentures" class="dentures">
             </div>
-            <h3>Other Care</h3>
+            <h3 class="serv-h3">Other Care</h3>
         </li>
     </ul>
 
